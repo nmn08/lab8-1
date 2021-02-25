52eb259e1564af15a8e5d7c7629da9d6ecc35449
Nhat Nguyen <nmn008@ucsd.edu>
2021-02-25 10:26:32 -0800

    Lab 8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'mffsGaJ4rE5p1sIFcqztNpSxJUTmbuuY';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+	center: [37.7749, -122.4194],
+	layers: L.mapquest.tileLayer('map'),
+	zoom: 12,
+	zoomControl: false
+	});
+
+	L.marker([32.87884383681223, -117.23586822388323]).addTo(map);
 }
